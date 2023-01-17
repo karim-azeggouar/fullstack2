3bb8636aa5c9bd608482a01b11fa0201578fb0f6
ntic <ntic.work@gmail.com>
2023-01-17 14:50:43 +0100

    mod1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <h1>Hello FullStack From React </h1>
+   <h1>Hello FullStack From React 204 -205 </h1>
     </div>
   );
 }
